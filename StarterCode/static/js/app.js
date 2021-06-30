6664b1d05c8d6af35157be036969848c89dd8ed8
ezapagon <ezapagon@gmail.com>
2021-06-29 22:45:01 -0500

    Rows

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
-var tableData = data;
+var tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
+console.log(data);
+// Loop through data and console.log each weather report object
+data.forEach(element => console.log(element));
+
+//append one table row `tr`for each weather report object
+
+data.forEach((element) => {
+    var row = tbody.append("tr")
+})
+
+
