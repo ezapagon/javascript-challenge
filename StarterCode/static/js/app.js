8de8070c556e9e81a922b801a81bda27c4b65270
ezapagon <ezapagon@gmail.com>
2021-06-30 01:55:21 -0500

    Notes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,18 +1,45 @@
+
 // from data.js
+var tableData = data;
+
 var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
-console.log(data);
-// Loop through data and console.log each weather report object
+// Loop through data and console.log each field
 data.forEach(element => console.log(element));
 
-//append one table row `tr`for each weather report object
+// Create the table using tableData set
+function buildTable(data){
+    
+    tbody.html("");
 
-data.forEach((element) => {
-    var row = tbody.append("tr");
-    Object.entries(element).forEach(([key,value]) => {
-        row.append("td").text(value);
+   data.forEach((element) => {
+      var row = tbody.append("tr");
+      Object.entries(element).forEach(([key,value]) => {
+      row.append("td").text(value);
     })
-})
+  });
 
+}
+// Complete the event handler function for the form
 
+function runEnter() {
+
+      d3.event.preventDefault();
+
+      var inputElement = d3.select("#datetime").property("value");
+
+      if(inputElement) {
+
+          // Get the value property of the input element
+        var filterData = tableData.filter((row) => row.datetime === inputElement);
+
+        console.log(filterData);
+
+      }
+
+      buildTable(filterData);
+}
+// Select the input element and get the raw HTML node and Create event handlers 
+d3.selectAll("#filter-btn").on('click', runEnter);
+
+buildTable(tableData);
